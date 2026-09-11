055fc94e05d3ca29123e34a8edb1eec8ebbd92b2
Tyler O'Farrell <tylerofarrell3650@gmail.com>
2026-09-11 15:58:52 +1000

    updated to 0.1.23-alpha

--- Casks/nuviodesktop.rb
+++ Casks/nuviodesktop.rb
@@ -1,7 +1,7 @@
 cask "nuviodesktop" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.1.22-alpha"
+  version "0.1.23-alpha"
   sha256 :no_check
 
   url "https://github.com/NuvioMedia/NuvioDesktop/releases/download/#{version}/Nuvio-macOS-#{arch}-#{version}.dmg"
