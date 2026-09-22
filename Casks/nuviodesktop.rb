36fbd6f2c160ac2c2a43504de68201baa0e3d280
Tyler O'Farrell <tylerofarrell3650@gmail.com>
2026-09-22 19:46:37 +1000

    updated to 0.1.25-alpha (postflight changed to postflight_steps too). idk if the postflight shit is necessary anymore tbh

--- Casks/nuviodesktop.rb
+++ Casks/nuviodesktop.rb
@@ -1,7 +1,7 @@
 cask "nuviodesktop" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.1.23-alpha"
+  version "0.1.25-alpha"
   sha256 :no_check
 
   url "https://github.com/NuvioMedia/NuvioDesktop/releases/download/#{version}/Nuvio-macOS-#{arch}-#{version}.dmg"
@@ -11,7 +11,7 @@ cask "nuviodesktop" do
 
   app "Nuvio.app"
 
-  postflight do
+  postflight_steps do
     ohai "Patching Nuvio with bash script (requires admin password)"
 
     system_command "/bin/bash",
