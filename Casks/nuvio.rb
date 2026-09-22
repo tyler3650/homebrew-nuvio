50c14922110dd13d8afe0cf514f099fe5269ae26
Tyler O'Farrell <tylerofarrell3650@gmail.com>
2026-09-22 21:07:22 +1000

    added paths to zap

--- Casks/nuvio.rb
+++ Casks/nuvio.rb
@@ -4,12 +4,23 @@ cask "nuvio" do
   version "0.1.25-alpha"
   sha256 :no_check
 
+  https://github.com/NuvioMedia/NuvioDesktop/releases/latest
+
   url "https://github.com/NuvioMedia/NuvioDesktop/releases/download/#{version}/Nuvio-macOS-#{arch}-#{version}.dmg"
   name "Nuvio Desktop"
   desc "Nuvio Desktop is a media client for browsing metadata, managing collections and watch progress, downloading media, and playing streams from user-installed extensions or user-provided sources."
   homepage "https://nuvio.tv/"
 
   app "Nuvio.app"
+
+  zap trash: [
+    "~/Library/Application Support/Nuvio",
+    "~/Library/Caches/com.nuvio.media.desktop",
+    "~/Library/Caches/Nuvio",
+    "~/Library/HTTPStorages/com.nuvio.media.desktop",
+    "~/Library/WebKit/com.nuvio.media.desktop",
+    "/private/var/folders/nz/nxxfvw7x6vjchgyr76bscbq80000gn/C/com.nuvio.media.desktop",
+  ]
 
   postflight_steps do
     run "/bin/bash",
